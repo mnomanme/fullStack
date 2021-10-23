10535042469ee70048b2f7be81bb62b0caf3afdc
nomanme <abdullah.noman083@gmail.com>
2021-10-23 23:14:59 +0600

    update folder name

---
+++ 65-genius-car-nodemongo-crud-recap/genius-car-client/src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,22 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyDW7jW45UyRpjTOCDn7hCo3P5fx9Ry9KLo',
+	authDomain: 'world-car-mechanics.firebaseapp.com',
+	projectId: 'world-car-mechanics',
+	storageBucket: 'world-car-mechanics.appspot.com',
+	messagingSenderId: '748970267171',
+	appId: '1:748970267171:web:8b2e73465007f479add796',
+};
+
+export default firebaseConfig;
+
+// console.log(process.env);
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_FIREBASE_APP_ID,
+// };
+
+// export default firebaseConfig;
