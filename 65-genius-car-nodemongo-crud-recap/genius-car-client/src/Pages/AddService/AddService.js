8d7d3b35dc33055bcc3a929d59fba72e5b90ab63
nomanme <abdullah.noman083@gmail.com>
2021-10-24 02:16:18 +0600

    add service form create

--- 65-genius-car-nodemongo-crud-recap/genius-car-client/src/Pages/AddService/AddService.js
+++ 65-genius-car-nodemongo-crud-recap/genius-car-client/src/Pages/AddService/AddService.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 export const AddService = () => {
+	const { register, handleSubmit } = useForm();
+
+	const onSubmit = (data) => {
+		console.log(data);
+	};
+
 	return (
-		<>
-			<h2>This is add service</h2>
-		</>
+		<section className="d-flex  justify-content-center my-5">
+			<section className="row w-25">
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<div className="form-group ">
+						<input {...register('name', { required: true, maxLength: 20 })} className="form-control " placeholder="name" />
+					</div>
+					<div className="form-group">
+						<input {...register('description')} className="form-control" placeholder="description" />
+					</div>
+					<div className="form-group">
+						<input type="number" {...register('price')} className="form-control" placeholder="price" />
+					</div>
+					<div className="form-group">
+						<input {...register('img')} className="form-control" placeholder="img url" />
+					</div>
+					<div className="form-group">
+						<input type="submit" value="Submit" className="form-control btn btn-success" />
+					</div>
+				</form>
+			</section>
+		</section>
 	);
 };
