8847a886cb662e892879d8f4d0073d9ade90b455
nomanme <abdullah.noman083@gmail.com>
2021-10-23 18:42:00 +0600

    update user

--- 64-mongodb-database-integration-crud/crud-client/src/components/UpdateUser/UpdateUser.js
+++ 64-mongodb-database-integration-crud/crud-client/src/components/UpdateUser/UpdateUser.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 
 export const UpdateUser = () => {
+	const { id } = useParams();
+
+	const [user, setUser] = useState({});
+
+	useEffect(() => {
+		const url = `http://localhost:5000/users/${id}`;
+		fetch(url)
+			.then((res) => res.json())
+			.then((data) => {
+				setUser(data);
+				console.log(data);
+			});
+	}, [id]);
+
 	return (
 		<>
-			<h2>This is update user</h2>
+			<h2>This is update user {id}</h2>
+			<h4>{user.name}</h4>
 		</>
 	);
 };
--- 64-mongodb-database-integration-crud/crud-client/src/components/Users/Users.js
+++ 64-mongodb-database-integration-crud/crud-client/src/components/Users/Users.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 export const Users = () => {
 	const [users, setUsers] = useState([]);
@@ -37,9 +38,11 @@ export const Users = () => {
 				return (
 					<Container key={user._id} className="text-center">
 						<strong>Name: {user.name}</strong> <small>Email: {user.email}</small>
-						<Button variant="outline-warning" size="sm" className="m-1">
-							update
-						</Button>
+						<Link to={`/users/update/${user._id}`}>
+							<Button variant="outline-warning" size="sm" className="m-1">
+								update
+							</Button>
+						</Link>
 						<Button onClick={() => handleDeleteUser(user._id)} variant="outline-danger" size="sm" className="m-1">
 							delete
 						</Button>
