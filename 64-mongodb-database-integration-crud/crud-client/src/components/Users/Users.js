1217d6627bfbdfb5b22b80f0131de0eff62650a7
nomanme <abdullah.noman083@gmail.com>
2021-10-23 17:15:41 +0600

    fetch database data

--- 64-mongodb-database-integration-crud/crud-client/src/components/Users/Users.js
+++ 64-mongodb-database-integration-crud/crud-client/src/components/Users/Users.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
 
 export const Users = () => {
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => {
+		fetch(`http://localhost:5000/users`)
+			.then((res) => res.json())
+			.then((data) => setUsers(data));
+	}, []);
+
 	return (
 		<>
-			<h2>This is Users</h2>
+			{/* <section class="w-25">
+				<div class="form-group">
+					<label for="Price">Price</label>
+					<input type="text" class="form-control" id="price" placeholder="price" />
+				</div>
+				<div class="form-group">
+					<label for="Quantity">Quantity</label>
+					<input type="text" class="form-control" id="quantity" placeholder="quantity" />
+				</div>
+				<button type="submit" class="btn btn-sm btn-outline-info">
+					Update Product
+				</button>
+			</section> */}
+			<h2>Users Available {users.length}</h2>
+			{users.map((user) => {
+				return (
+					<Container key={user._id} className="text-center">
+						<strong>Name: {user.name}</strong> <small>Email: {user.email}</small>
+						<button className="btn btn-sm btn-outline-warning m-2">update</button>
+						<button className="btn btn-sm btn-outline-danger">delete</button>
+					</Container>
+				);
+			})}
 		</>
 	);
 };
