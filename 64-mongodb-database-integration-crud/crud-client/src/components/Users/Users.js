00a0c1344296b30b0e09923ffbe4c2c6cf9ee80b
nomanme <abdullah.noman083@gmail.com>
2021-10-23 18:16:41 +0600

    delete user successfully

--- 64-mongodb-database-integration-crud/crud-client/src/components/Users/Users.js
+++ 64-mongodb-database-integration-crud/crud-client/src/components/Users/Users.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 
 export const Users = () => {
 	const [users, setUsers] = useState([]);
@@ -10,28 +10,39 @@ export const Users = () => {
 			.then((data) => setUsers(data));
 	}, []);
 
+	// DELETE AN USER
+	const handleDeleteUser = (id) => {
+		const proceed = window.confirm('Are you sure? you want to delete?');
+
+		if (proceed) {
+			const url = `http://localhost:5000/users/${id}`;
+			fetch(url, {
+				method: 'DELETE',
+			})
+				.then((res) => res.json())
+				.then((data) => {
+					if (data.deletedCount > 0) {
+						alert('User deleted successfully');
+						const remainingusers = users.filter((user) => user._id !== id);
+						setUsers(remainingusers);
+					}
+				});
+		}
+	};
+
 	return (
 		<>
-			{/* <section class="w-25">
-				<div class="form-group">
-					<label for="Price">Price</label>
-					<input type="text" class="form-control" id="price" placeholder="price" />
-				</div>
-				<div class="form-group">
-					<label for="Quantity">Quantity</label>
-					<input type="text" class="form-control" id="quantity" placeholder="quantity" />
-				</div>
-				<button type="submit" class="btn btn-sm btn-outline-info">
-					Update Product
-				</button>
-			</section> */}
 			<h2>Users Available {users.length}</h2>
 			{users.map((user) => {
 				return (
 					<Container key={user._id} className="text-center">
 						<strong>Name: {user.name}</strong> <small>Email: {user.email}</small>
-						<button className="btn btn-sm btn-outline-warning m-2">update</button>
-						<button className="btn btn-sm btn-outline-danger">delete</button>
+						<Button variant="outline-warning" size="sm" className="m-1">
+							update
+						</Button>
+						<Button onClick={() => handleDeleteUser(user._id)} variant="outline-danger" size="sm" className="m-1">
+							delete
+						</Button>
 					</Container>
 				);
 			})}
