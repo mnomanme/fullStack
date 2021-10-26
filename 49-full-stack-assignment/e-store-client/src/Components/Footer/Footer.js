9e47965ad3c5b67679603cf675203da439e66c9d
nomanme <abdullah.noman083@gmail.com>
2021-10-26 23:32:31 +0600

    footer done

--- 49-full-stack-assignment/e-store-client/src/Components/Footer/Footer.js
+++ 49-full-stack-assignment/e-store-client/src/Components/Footer/Footer.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
-const Footer = () => {
+export const Footer = () => {
+	const footer = {
+		textAlign: 'center',
+		color: 'teal',
+		marginTop: '50px',
+	};
 
-    const footer={
-        textAlign: "center",
-        color: "teal",
-        marginTop: "50px",
-    }
-
-    return (
-        <div style={footer}>
-            <p> &copy; <a href="https://www.linkedin.com/in/naimul-alam/" target="_blank" rel="noreferrer">Naimul Alam</a> || All rights reserved <small> 2021 </small></p>
-        </div>
-    );
+	return (
+		<div style={footer}>
+			<p>
+				&copy;
+				<a href="https://www.linkedin.com/in/mohammadnomanme/" target="_blank" rel="noreferrer">
+					Mohammad Noman
+				</a>
+				|| All rights reserved <small> 2021 </small>
+			</p>
+		</div>
+	);
 };
-
-export default Footer;
