fe1eb79de29fe614c2e8489dd813223b82a8c29d
nomanme <abdullah.noman083@gmail.com>
2021-10-31 23:43:48 +0600

    dependecies & form add

--- 48.5-deploy-to-heroku-and-practice-problem/volunteer-network-client/src/App.js
+++ 48.5-deploy-to-heroku-and-practice-problem/volunteer-network-client/src/App.js
@@ -1,9 +1,42 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Home } from './components/Home/Home';
+import { AddEvents } from './components/AddEvents/AddEvents';
 
 function App() {
 	return (
 		<div className="App">
 			<h3>Volunteer Network</h3>
+			<Router>
+				<div>
+					<nav>
+						<ul>
+							<li>
+								<Link to="/">Home</Link>
+							</li>
+							<li>
+								<Link to="/addEvents">Add Events</Link>
+							</li>
+							<li>
+								<Link to="/dashboard">Dashboard</Link>
+							</li>
+						</ul>
+					</nav>
+
+					<Switch>
+						<Route exact path="/">
+							<Home />
+						</Route>
+						<Route path="/addEvents">
+							<AddEvents />
+						</Route>
+
+						<Route path="/dashboard">
+							<Home />
+						</Route>
+					</Switch>
+				</div>
+			</Router>
 		</div>
 	);
 }
