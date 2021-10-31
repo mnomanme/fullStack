170c6b38c76013b563ac76194795432dd2a18c6c
nomanme <abdullah.noman083@gmail.com>
2021-10-31 23:43:24 +0600

    db setup

--- 48.5-deploy-to-heroku-and-practice-problem/volunteer-network-server/server.js
+++ 48.5-deploy-to-heroku-and-practice-problem/volunteer-network-server/server.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
+app.use(cors);
+app.use(express.json());
+
 const port = process.env.PORT || 45000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@volunteer.lrgaq.mongodb.net/${process.env.DB_Name}?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
 });
 
+client.connect((err) => {
+	console.log('connection error:', err);
+	const eventsCollection = client.db('volunteer').collection('events');
+	console.log('Volunteer Database Connected Successfully!');
+});
+
 app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`);
+	console.log(`Volunteer Port Running Now:${port}`);
 });
