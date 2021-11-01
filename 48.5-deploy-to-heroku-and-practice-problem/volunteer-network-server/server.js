bb5c3872bb8fc6d36bcd5d66ffbeb47995947389
nomanme <abdullah.noman083@gmail.com>
2021-11-02 00:18:29 +0600

    delete

--- 48.5-deploy-to-heroku-and-practice-problem/volunteer-network-server/server.js
+++ 48.5-deploy-to-heroku-and-practice-problem/volunteer-network-server/server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const ObjectID = require('mongodb').ObjectId;
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
+const { ObjectID } = require('bson');
 
 app.use(cors);
 app.use(express.json());
 
-const port = process.env.PORT || 45000;
+const port = 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@volunteer.lrgaq.mongodb.net/${process.env.DB_Name}?retryWrites=true&w=majority`;
 
@@ -21,6 +23,31 @@ client.connect((err) => {
 	console.log('connection error:', err);
 	const eventsCollection = client.db('volunteer').collection('events');
 	console.log('Volunteer Database Connected Successfully!');
+
+	// get
+
+	app.get('/events', (req, res) => {
+		eventsCollection.find().toArray((err, items) => {
+			console.log('from db', items);
+			res.send(items);
+		});
+	});
+
+	// post
+	app.post('/addEvent', (req, res) => {
+		const newEvent = req.body;
+		console.log('adding new event', newEvent);
+		eventsCollection.insertOne(newEvent).then((result) => {
+			console.log('inserted count', insertedCount);
+			res.send(result.insertedCount > 0);
+		});
+	});
+
+	app.delete('/deleteEvent/:id', (req, res) => {
+		const id = ObjectID(req.params.id);
+		console.log('delete this', id);
+		eventsCollection.findOneAndDelete({ _id: id }).then((documents) => res.send(documents.value));
+	});
 });
 
 app.listen(port, () => {
