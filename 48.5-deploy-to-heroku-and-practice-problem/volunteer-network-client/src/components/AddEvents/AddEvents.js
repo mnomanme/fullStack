f555c17ba3c621c2cf5b31bebb1b12950bfed6a7
nomanme <abdullah.noman083@gmail.com>
2021-11-01 23:23:12 +0600

    img set live db

--- 48.5-deploy-to-heroku-and-practice-problem/volunteer-network-client/src/components/AddEvents/AddEvents.js
+++ 48.5-deploy-to-heroku-and-practice-problem/volunteer-network-client/src/components/AddEvents/AddEvents.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import axios from 'axios';
 
 export const AddEvents = () => {
 	const {
@@ -15,6 +16,16 @@ export const AddEvents = () => {
 		console.log(e.target.files[0]);
 		const imgData = new FormData();
 		imgData.set('key', '6bc6ba0d735eda3fbf193407b965ff2b');
+		imgData.append('image', e.target.files[0]);
+
+		axios
+			.post('https://api.imgbb.com/1/upload', imgData)
+			.then((res) => {
+				console.log(res.data.data.display_url);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
 	};
 
 	console.log(watch('example')); // watch input value by passing the name of it
